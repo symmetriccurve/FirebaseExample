4086781c6dee99535a07b37f35db9fe9312173e0
SymCurve <symmetriccurve@gmail.com>
2016-04-04 00:30:32 -0400

    C Made some Changes and Committed to remote 12:30 am

--- index.ios.js
+++ index.ios.js
@@ -70,7 +70,8 @@ removeTodo(rowData) {
 
 render() {
  //B Made Changes and Committed to remote 12:23 am
- //B Made some more Changes and Committed to remote 12:24 am
+ //B Made some more Changes and Committed to remote 12:24 
+ //C Made Changes and Committed to remote 12:30 am
   return (
     <View style={styles.appContainer}>
       <View style={styles.titleView}>
